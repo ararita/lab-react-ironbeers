de3a2a7aab62084ceb51286280c14af055c0b2fe
ararita <munjizavesna@gmail.com>
2020-12-04 15:45:36 +0100

    Iteration 1 done

---
+++ src/components/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Home = () => {
+  return (
+    <div ClassName="home">
+      <Link to="/beers">All Beers</Link>
+      <Link to="/random-beers">Random Beers</Link>
+      <Link to="/new-beer">New Beer</Link>
+    </div>
+  );
+};
+
+export default Home;
